7186a18a032581265b69bc865913700ff4339e27
amey <amey1512@gmail.com>
2020-08-18 01:56:20 +0530

    created and run three home/post/profile routes

---
+++ src/components/post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Post = () => {
+  return(
+    <div>
+      Hey we are inside Post component.
+    </div>
+  )
+}
+
+export default Post;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 
+import Home from './components/home';
+import Profile from './components/profile';
+import Post from './components/post';
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <div>
+        <Route exact path="/"   component={Home}/>
+        <Route path="/profile"  component={Profile}/>
+        <Route path="/posts"    component={Post}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
